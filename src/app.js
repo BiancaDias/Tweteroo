c33aed39b92bca125151348244d80d7777d8c0d0
Bianca Dias <bianca.dias@ufv.br>
2023-04-06 10:38:42 -0300

    requisito post '/sign-up1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from "express";
 import { tweets } from "./tweets.js";
+import cors from "cors";
 
 const app = express();
 app.listen(5000, ()=>console.log("servidor online"));
+app.use(express.json()); //para ler arquivos recebidos como json
+app.use(cors()); // para permitir comunicação com o front
+
+const users = []
+app.post("/sign-up", (req, res) => {
+    const {username, avatar} = req.body;
+    users.push({username, avatar});
+    res.send("OK");
+})
 
 
 app.get("/tweets", (req,res) =>{
